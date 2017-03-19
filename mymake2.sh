5f253e614d004e1bd103153f4385d8f06a491f80
jjhoekstra <canterstate69@yahoo.com>
2017-03-19 18:10:58 +0100

    Add files via upload

--- mymake2.sh
+++ mymake2.sh
@@ -1,6 +1,6 @@
 
 as -mcpu=cortex-a53  -mfpu=vfpv4 jh4th2ra.s -o jh4th2ra.o
 ld -T lscript jh4th2ra.o -o jh4th2ra.elf
-objcopy jh4th2ra.elf -O ihex jh4th2ra.hex
+objcopy jh4th2ra.elf -O binary jh4th2ra.img
 
-echo "compiling for pi3 - done!"
+echo "compiling binary image for pi3 - done!"
